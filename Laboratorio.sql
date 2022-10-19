32e2f614e2a5c607e470b36a5fc5dac164e1c9fe
luis <luis.moreno.a7x@outlook.com>
2022-10-18 20:06:02 -0500

    Script

---
+++ Laboratorio.sql
@@ -0,0 +1,141 @@
+CREATE DATABASE "Laboratorio"
+    WITH
+    OWNER = postgres
+    TEMPLATE = template1
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Mexico.1252'
+    LC_CTYPE = 'Spanish_Mexico.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = false;
+
+CREATE SCHEMA Persona
+CREATE SCHEMA Aula
+
+CREATE TABLE Persona.Empleado
+(
+	RPE_Empleado BIGSERIAL NOT NULL,
+	Nombre VARCHAR(100) NOT NULL,
+	Domicilio VARCHAR(200) NOT NULL,
+	Correo VARCHAR(200) NOT NULL,
+	Celular VARCHAR(10) NOT NULL,
+	EmpleadoDesde DATE NOT NULL,
+	Antiguedad VARCHAR(20) NOT NULL,
+	TipoEmpleado VARCHAR(40) NOT NULL,
+
+	CONSTRAINT PK_EMPLEADO PRIMARY KEY (RPE_Empleado)
+)
+
+CREATE TABLE Persona.Colaborador
+(
+	RPE_Colaborador BIGSERIAL NOT NULL,
+	Desc_act TEXT NOT NULL,
+	Hrs_sem BIGINT NOT NULL,
+
+	CONSTRAINT FK_COLABRADOR FOREIGN KEY (RPE_Colaborador)
+		REFERENCES Persona.Empleado(RPE_Empleado)
+		ON DELETE CASCADE			
+)
+
+CREATE TABLE Persona.Becario
+(
+	RPE_Becario BIGSERIAL NOT NULL,
+	Fecha_nac DATE NOT NULL,
+	Hrs_sem BIGINT NOT NULL,
+	Generacion VARCHAR(10) NOT NULL,
+
+	CONSTRAINT FK_BECARIO FOREIGN KEY (RPE_Becario)
+			REFERENCES Persona.Empleado(RPE_Empleado)
+			ON DELETE CASCADE
+)
+
+CREATE TABLE Persona.Responsable
+(
+	RPE_Responsable BIGSERIAL NOT NULL,
+	Antiguedad DATE NOT NULL,
+	Grado VARCHAR(20) NOT NULL,
+	Fecha_Inicio DATE NOT NULL,
+	Fecha_Fin DATE NOT NULL,
+
+	CONSTRAINT FK_RESPONSABLE FOREIGN KEY (RPE_Responsable)
+			REFERENCES Persona.Empleado(RPE_Empleado)
+			ON DELETE CASCADE
+)
+
+CREATE TABLE Persona.Alumno
+(
+	Clave_Unica BIGSERIAL NOT NULL,
+	Nombre VARCHAR(40) NOT NULL,
+	Generacion BIGINT NOT NULL,
+	Carrera VARCHAR(20) NOT NULL,
+	Adeudo BIGINT NOT NULL,
+
+	CONSTRAINT PK_CLAVE PRIMARY KEY (Clave_Unica)
+)
+
+CREATE TABLE Aula.Equipo
+(
+	NumInv BIGSERIAL NOT NULL,
+	Nombre VARCHAR(40) NOT NULL,
+	Modelo VARCHAR(20) NOT NULL,
+	Descripcion VARCHAR(100) NOT NULL,
+	UbicacionEnLab VARCHAR(30) NOT NULL,
+	Marca VARCHAR(20) NOT NULL,
+	TipoEquipo VARCHAR(10) NOT NULL,
+
+	CONSTRAINT PK_NUMINV PRIMARY KEY (NumInv)
+
+)
+
+CREATE TABLE Aula.Sancion
+(
+	Clave_Unica BIGSERIAL NOT NULL,
+	RPE_Empleado BIGINT NOT NULL,
+	Descripcion VARCHAR(50) NOT NULL,
+	F_liquidacion DATE NOT NULL,
+	Fecha DATE NOT NULL,
+	Monto BIGINT NOT NULL,
+
+	CONSTRAINT FK_EMPLEADO FOREIGN KEY (RPE_Empleado)
+			REFERENCES Persona.Empleado(RPE_Empleado),
+	CONSTRAINT FK_CLAVE FOREIGN KEY (Clave_Unica)
+			REFERENCES Persona.Alumno(Clave_Unica)
+)
+
+CREATE TABLE Aula.Prestamo
+(
+	Id_Prestamo BIGSERIAL NOT NULL,
+	NumInv BIGINT NOT NULL,
+	Clave_Unica BIGINT NOT NULL,
+	RPE_Empleado BIGINT NOT NULL,
+	FechaEntrega DATE NOT NULL,
+	FechaPrestamo DATE NOT NULL,
+
+	CONSTRAINT PK_IDPRESTAMO PRIMARY KEY (Id_Prestamo),
+
+	CONSTRAINT FK_NUMINV FOREIGN KEY(NumInv)
+		REFERENCES Aula.Equipo(NumInv),
+	CONSTRAINT FK_CL FOREIGN KEY(Clave_Unica)
+		REFERENCES Persona.Alumno(Clave_Unica),
+	CONSTRAINT FK_EMPLE FOREIGN KEY(RPE_Empleado)
+		REFERENCES Persona.Empleado(RPE_Empleado)
+)
+
+CREATE TABLE Aula.BitacoraEntrega
+(
+	Id_Prestamo BIGSERIAL NOT NULL,
+	RPE_Empleado BIGINT NOT NULL,
+	Fecha_Entrega DATE NOT NULL,
+
+	CONSTRAINT FK_IDPRESTAMO FOREIGN KEY (Id_Prestamo)
+		REFERENCES Aula.Prestamo (Id_Prestamo)
+)
+
+CREATE TABLE Aula.Materia
+(
+	ClaveMateria BIGSERIAL,
+	Nombre VARCHAR(100) NOT NULL,
+	Nivel BIGINT NOT NULL,
+	
+	CONSTRAINT PK_CLAVEMATERIA PRIMARY KEY (ClaveMateria)	
+)
